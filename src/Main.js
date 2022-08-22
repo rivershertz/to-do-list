0415a324dee415c59e49eeae28d7859b8b806380
Nahar Shertz <river16@gmail.com>
2022-08-22 15:10:21 +0300

    first time with tailwind

---
+++ src/Main.js
@@ -0,0 +1,10 @@
+function Main () {
+    return (
+        <div className="w-1/2 mx-auto">
+            <h1 className="text-6xl text-center mb-16">The Things I Do</h1>
+            <div className="bg-red-500 h-16" />
+        </div>
+    )
+}
+
+export default Main
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    colors: {
+      'gradient': 'linear-gradient(360deg, rgba(0,255,218,1) 0%, rgba(0,134,255,1) 100%)'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
